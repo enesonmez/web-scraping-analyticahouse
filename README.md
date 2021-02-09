0b1ccf5039ebdfd1a570b234da41e6b77f0161e5
enesonmez <sonmezens@hotmail.com>
2021-02-09 18:23:05 +0300

    Complated data scraping with html source

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# web-scraping-analyticahouse
+# Web Scraping Analyticahouse Case
+Lets say one of our customers demanded a Product Report which consisting of product code, product name, product brand, product availability( as a percent ) , product price for all of products they have.<br>
+
+Your mission is build a crawling system which will go to those URLs (There is an xlsx file at the attachment), scrape necessary metrics and write to Excel as seperate columns. At the below picture all metrics can be seen.<br>
+
+![](files/image.png)
+
+Note: You should append www.spx.com.tr to every path for reaching page.<br>
+
+Bir müşterimizin ürün kodu, ürün adı, ürün markası, ürün bulunabilirliği (yüzde olarak), sahip oldukları tüm ürünler için ürün fiyatını içeren bir Ürün Raporu talep ettiğini varsayalım.<br>
+
+Göreviniz, bu URL'lere gidecek (ekte bir xlsx dosyası var), gerekli ölçütleri kazıyacak ve ayrı sütunlar halinde Excel'e yazacak bir tarama sistemi oluşturmaktır. Aşağıdaki resimde tüm ölçüler görülebilir.<br>
+
+Not: Her sayfaya ulaşmak için www.spx.com.tr'yi eklemelisiniz.
+
+## [Data Scraping by Downloading The Html Source](https://github.com/enesonmez/GlobalAIHubPythonHomework/blob/master/Homework-1/takeUserInputs.py)
+
+### Used Tech-Stack (Kullanılan Teknolojiler)
+* python 3.7.9
+* requests
+* beautifulsoup4
+* pandas
+* tqdm
+
+## [Data Scraping with Bot](https://github.com/enesonmez/GlobalAIHubPythonHomework/blob/master/Homework-1/takeUserInputs.py)
+
+### Used Tech-Stack
--- app/spx_com_web_scraping_bs4.py
+++ app/spx_com_web_scraping_bs4.py
@@ -1,3 +1,101 @@
+"""
+    author : Enes Sönmez
+"""
+
+# web scraping
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+
+# other processes
+from tqdm import tqdm
+import sys
+import locale
+
+class SpxScraperBS:
+    def __init__(self, *args, **kwargs):
+        self.__base_url = "https://www.spx.com.tr"
+
+    # The data in the file is converted into url.
+    def get_product_urls(self, filename):
+        try:
+            product_urls = pd.read_excel(filename, sheet_name=None, header=None)
+            product_urls = list(product_urls["Sheet4"].iloc[:,0])
+            product_urls = list(map(lambda url : self.__base_url + url, product_urls))
+            return product_urls
+        except Exception:
+            sys.exit("Filename is wrong or not expected file!!!")
+    
+    # A get request is sent to the url and the html source file is received.
+    def get_source(self, url):
+        r = requests.get(url)
+        if r.status_code == 200:
+            return BeautifulSoup(r.content, "lxml")
+        return False
+
+    # The product brand is returned.
+    def __get_product_brand(self, source):
+        try:
+            return source.find("a", attrs={"class": "product__brand"}).text
+        except Exception:
+            return "None"
+    
+    # The product name is returned. source => html page source (BeautifulSoup object)
+    def __get_product_name(self, source):
+        try:
+            return source.find("h1", attrs={"class": "product__title"}).text
+        except Exception:
+            return "None"
+    
+    # The product code is returned.
+    def __get_product_code(self, source):
+        try:
+            return source.find("div", attrs={"class": "product__code"}).text
+        except Exception:
+            return "None"
+    
+    # The product price is returned.
+    def __get_product_price(self, source):
+        try:
+            return source.find("div", attrs={"class": "product__price"}).text
+        except Exception:
+            return "None"
+    
+    # The product availability percent is returned. (sold out product type / total listed product type)
+    def __get_product_availability(self, source):
+        try:
+            total_product_type = len(source.find("div", attrs={"data-variant-key": "integration_first_size"}).find_all("a"))
+            soldout_product_type = len(source.find("div", attrs={"data-variant-key": "integration_first_size"}).find_all("a", attrs={"class": "disabled"}))
+            return (soldout_product_type/total_product_type) * 100
+        except Exception:
+            return "None"
+
+    def scrape_products(self, filename):
+        # money to float convert
+        locale.setlocale(locale.LC_ALL, 'en_US.UTF8')
+        data = list()
+        urls = self.get_product_urls(filename)
+        # to see work progress
+        bar = tqdm(urls, unit="product")
+        for url in bar:
+            bar.set_description(url)
+            product_source = self.get_source(url)
+            brand = self.__get_product_brand(product_source)
+            name = self.__get_product_name(product_source).strip() # Spaces at the beginning and end of the string are deleted.
+            code = self.__get_product_code(product_source).strip()
+            price = locale.atof(self.__get_product_price(product_source).strip().replace("TL", "")) # money to float convert
+            availability_percent = self.__get_product_availability(product_source)
+            data.append((brand, name, code, price, availability_percent))
+        return data
+    
+    # Data is written to the excel file. data => list()
+    def write_excel(self, data, filename="products_info.xlsx"):
+        df = pd.DataFrame(data)
+        df.columns = ["Product_Brand", "Product_Name", "Product_Code", "Product_Price_TL", "Product_Availability_Percent"]
+        df = df.drop_duplicates() # Duplicate data is deleted.
+        df.to_excel(filename)
+
+if __name__ == '__main__':
+    spx = SpxScraperBS()
+    data = spx.scrape_products("../files/Product-Detail-URL.xlsx")
+    spx.write_excel(data)
